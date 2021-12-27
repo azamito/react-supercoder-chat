8a3c49576ad0f91bb5a1aa07b0eea4e764bbb013
azamito <zamiaswad@gmail.com>
2021-12-27 16:39:31 +0700

    persiapan pwa

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import * as ServiceWorker from './serviceWorkers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ServiceWorker.register();
